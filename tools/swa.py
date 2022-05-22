7c0fa1ca0c5ca14a3dab2a097e470031a7557cef
zjw97 <1018510885@qq.com>
2022-05-23 01:46:02 +0800

    model merge: swa

--- tools/swa.py
+++ tools/swa.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Time : 6/16/21 10:33 AM
-# @Author :Jupiter(duping@www.mainto.cn)
-# @Email : qiuyanxinjupiter2013@gmail.com
-# @File : swa_saved_model.py
 import os
 import torch
 from os.path import join as opj
@@ -24,7 +19,7 @@ def swa(model_paths,best_n_model,save_dir,ckpt_dir,zipfile):
     model_names = [i.split('/')[-1] for i in model_paths]
     print(f'load model {model_names}')
 
-    models = [torch.load(model_path,map_location='cpu') for model_path in model_paths]
+    models = [torch.load(model_path, map_location='cpu') for model_path in model_paths]
     model_num = len(models)
     model_keys = models[-1]['state_dict'].keys()
     state_dict = models[-1]['state_dict']
@@ -67,8 +62,8 @@ if __name__ == '__main__':
         '/home/hanglijun/xp/code/mmdetection/work_dirs/swinb_3x_800-1400_anchor_bs2x8_anchor_aug_iou_ad_2048'
         , help='the directory where checkpoints are saved')
     parser.add_argument('--best_n_model', default=5, help='select best n models for ensemble')
-    parser.add_argument('--indexs', default=[21,22,23,24,25], help='select best n models for ensemble')
+    parser.add_argument('--indexs', default=[21, 22, 23, 24, 25], help='select best n models for ensemble')
     parser.add_argument('--save_dir', default=None, help='the directory for saving the SWA model')
     args = parser.parse_args()
 
-    swabyindex(args.ckpt_dir, args.indexs,args.best_n_model, save_dir=args.save_dir, zipfile=False)
+    swabyindex(args.ckpt_dir, args.indexs, args.best_n_model, save_dir=args.save_dir, zipfile=False)
